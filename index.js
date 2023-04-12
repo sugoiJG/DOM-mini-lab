f69e463cad0f0e0c3a2e2eb804cdd9bb782651a5
Jason Golliet <jgolliet@LABA.PSWAS.LOCAL>
2023-04-12 16:08:31 -0700

    finished problem

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
+// document.getElementById('title').innerHTML = "some text";
 
-let googleLink = document.getElementsByTagName(a)
-console.log(googleLink)
+// let hideButton = document.getElementById('hideButton');
+// let unhideButton = document.getElementById('unhideButton');
 
-googleLink.href = "https://www.google.com"
+// hideButton.addEventListener('click', (e) => {
+//     console.log("button was pressed");
+// })
+
+// let hideButton = () => {
+//     let title = document.getElementById('title');
+//     title.style.display = "none";
+// }
+
+// let unhideButton = () => {
+//     let title = document.getElementById('title');
+//     title.style.display = "none";
+// }
+
+
+h1Element = document.querySelector('h1');
+hideButtonElement = document.getElementById('hideButton');
+unhideButtonElement = document.getElementById('unhideButton');
+console.log(h1Element, hideButtonElement, unhideButtonElement)
+
+h1Element.innerHTML = "some text";
+
+hideButtonElement.addEventListener('click', () => {
+h1Element.style.display = "none";
+})
+
+unhideButtonElement.addEventListener('click', () => {
+    h1Element.style.display = "inline";
+    })
+    
